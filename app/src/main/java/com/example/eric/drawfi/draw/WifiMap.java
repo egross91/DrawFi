9da7445f76a85592fce4d007d3e0e1421d2bda69
Eric <eric.bgross@gmail.com>
2015-03-13 22:12:36 -0400

    for Aleksey <3

--- app/src/main/java/com/example/eric/drawfi/draw/WifiMap.java
+++ app/src/main/java/com/example/eric/drawfi/draw/WifiMap.java
@@ -1,5 +1,15 @@
 package com.example.eric.drawfi.draw;
 
-public class WifiMap {
+import android.graphics.Bitmap;
 
+public class WifiMap {
+    private int[][] visited;
+    private int center;
+    private Bitmap map;
+
+    public WifiMap(int rows, int cols) {
+        this.visited = new int[rows][cols];
+        this.center = (rows < cols) ? cols >> 1 : rows >> 1;
+        this.map = Bitmap.createBitmap(cols, rows, Bitmap.Config.RGB_565);
+    }
 }
